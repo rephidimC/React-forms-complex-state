b014c30ca8a949aa09fbf8726a83eeeffa378bc2
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-05-22 03:27:09 +0100

    added finished files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  var [iname, setIname] = useState();
+
+  function submitted(event) {
+    setIname(fname + " " + lname);
+    event.preventDefault();
+  }
+  function firstName(event) {
+    setFname(event.target.value);
+  }
+  function lastName(event) {
+    setLname(event.target.value);
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
-      <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+      <h1>Hello {iname}</h1>
+      <form onSubmit={submitted}>
+        <input onChange={firstName} name="fName" placeholder="First Name" />
+        <input onChange={lastName} name="lName" placeholder="Last Name" />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
